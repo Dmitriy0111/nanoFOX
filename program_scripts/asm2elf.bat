8799f2d1c8312bd69b3ff1706702ac627940b56f
Dmitriy0111 <vlasovdv0111@mail.ru>
2018-12-15 22:00:39 +0700

    Added readme file and edit makefile

--- program_scripts/asm2elf.bat
+++
@@ -1,3 +0,0 @@
-riscv-none-embed-gcc main.S -c -o main -march=rv64ima -mabi=lp64
-riscv-none-embed-objdump -D -z main > main.elf
-erase main
--- program_scripts/asm2elf.sh
+++
@@ -1,3 +0,0 @@
-riscv64-unknown-elf-gcc main.S -c -o main -march=rv64ima -mabi=lp64
-riscv64-unknown-elf-objdump -D -z main > main.elf
-rm main
